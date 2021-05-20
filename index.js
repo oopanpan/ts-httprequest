f7a53603e91c426f8ad796aa9fd12f05bff7e0de
oopanpan <oopanpan@gmail.com>
2021-05-19 23:48:46 -0700

    interface example

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 exports.__esModule = true;
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/todos/1';
-axios_1["default"].get(url).then(function (r) { return console.log(r.data); });
+axios_1["default"].get(url).then(function (r) {
+    var todo = r.data;
+    var id = todo.id;
+    var title = todo.title;
+    var completed = todo.completed;
+    console.log("\n        The Todo with ID: " + id + "\n        Has a title of: " + title + "\n        Is it finished? " + completed + "\n    ");
+});
--- index.ts
+++ index.ts
@@ -2,4 +2,22 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-axios.get(url).then((r) => console.log(r.data));
+interface Todo {
+	id: number;
+	title: string;
+	completed: boolean;
+}
+
+axios.get(url).then((r) => {
+	const todo = r.data as Todo;
+
+	const id = todo.id;
+	const title = todo.title;
+	const completed = todo.completed;
+
+	console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished? ${completed}
+    `);
+});
